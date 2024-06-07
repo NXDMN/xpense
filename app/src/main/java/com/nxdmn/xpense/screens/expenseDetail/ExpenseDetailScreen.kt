4793cd7f16abe5ebeb7c1a092058bf8941b50661
NXDMN <norvintsl@gmail.com>
2024-06-07 23:46:06 +0800

    Create CurrencyTextField

--- app/src/main/java/com/nxdmn/xpense/screens/expenseDetail/ExpenseDetailScreen.kt
+++ app/src/main/java/com/nxdmn/xpense/screens/expenseDetail/ExpenseDetailScreen.kt
@@ -6,19 +6,30 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Label
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDatePickerState
@@ -28,12 +39,20 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import androidx.core.text.isDigitsOnly
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -41,6 +60,7 @@ import com.nxdmn.xpense.data.models.ExpenseModel
 import com.nxdmn.xpense.helpers.readImage
 import com.nxdmn.xpense.helpers.readImageFromPath
 import com.nxdmn.xpense.helpers.toEpochMilli
+import com.nxdmn.xpense.ui.components.CurrencyTextField
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -74,13 +94,16 @@ fun ExpenseDetailScreen(
         },
     ) { innerPadding ->
         Column(
-            modifier = Modifier.padding(innerPadding),
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(10.dp),
             verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            var amount by remember { mutableStateOf(expense.amount.toString()) }
-            TextField(
-                value = amount,
-                onValueChange = {
+            var amount by remember { mutableStateOf(if(expense.amount == 0.0) "" else expense.amount.toString()) }
+            CurrencyTextField(
+                amount = amount,
+                onValueChanged = {
                     amount = if (it.isEmpty()){
                         it.trim()
                     } else {
@@ -90,9 +113,6 @@ fun ExpenseDetailScreen(
                         }
                     }
                 },
-                label = { Text("Amount") },
-                singleLine = true,
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
             )
 
             val datePickerState = rememberDatePickerState(initialSelectedDateMillis = expense.date.toEpochMilli())
@@ -157,8 +177,74 @@ fun ExpenseDetailScreen(
     }
 }
 
-@Preview
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(device = Devices.PIXEL_7_PRO)
 @Composable
 fun TestPreview() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("Expense")
+                },
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+            )
+        },
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(10.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CurrencyTextField(
+                amount = "",
+                onValueChanged = {},
+            )
 
+            DatePicker(
+                state = rememberDatePickerState(),
+                modifier = Modifier.padding(16.dp),
+                title = null,
+                headline = null,
+                showModeToggle = false,
+                colors = DatePickerDefaults.colors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                )
+            )
+
+            TextField(
+                value = "",
+                onValueChange = { },
+                label = { Text("Category") }
+            )
+
+            TextField(
+                value = "",
+                onValueChange = { },
+                label = { Text("Remarks") }
+            )
+
+
+            Button(onClick = {}) {
+                Text("Add Image")
+            }
+
+            Button(onClick = {}) {
+                Text("Add")
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/nxdmn/xpense/ui/components/CurrencyTextField.kt
@@ -0,0 +1,51 @@
+package com.nxdmn.xpense.ui.components
+
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CurrencyTextField(amount: String, onValueChanged: (String) -> Unit){
+    TextField(
+        value = amount,
+        onValueChange = onValueChanged,
+        modifier = Modifier.padding(40.dp).width(IntrinsicSize.Min),
+        textStyle = MaterialTheme.typography.headlineSmall.copy(textAlign = TextAlign.Center),
+        placeholder = {
+            Text(
+                text = "Amount",
+                modifier = Modifier.fillMaxWidth(),
+                color = Color.Gray,
+                style = MaterialTheme.typography.headlineSmall.copy(textAlign = TextAlign.Center),
+            )
+        },
+        prefix = {
+            Text(
+                text = "SGD",
+                modifier = Modifier.padding(end = 16.dp),
+                style = MaterialTheme.typography.headlineSmall
+            )
+        },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        shape = RoundedCornerShape(30.dp),
+        colors = TextFieldDefaults.colors(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+    )
+}
