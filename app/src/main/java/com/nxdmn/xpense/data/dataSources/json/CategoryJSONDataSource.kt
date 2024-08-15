17edbf1745389b539842c13ea8ab21cc154e4cfb
NXDMN <norvintsl@gmail.com>
2024-08-15 23:37:29 +0800

    Move generateDefaultList to repository

--- app/src/main/java/com/nxdmn/xpense/data/dataSources/json/CategoryJSONDataSource.kt
+++ app/src/main/java/com/nxdmn/xpense/data/dataSources/json/CategoryJSONDataSource.kt
@@ -6,7 +6,6 @@ import com.nxdmn.xpense.data.models.CategoryModel
 import com.nxdmn.xpense.helpers.exists
 import com.nxdmn.xpense.helpers.read
 import com.nxdmn.xpense.helpers.write
-import com.nxdmn.xpense.ui.CategoryIcon
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import java.util.Random
@@ -22,10 +21,7 @@ class CategoryJSONDataSource(private val context: Context) : CategoryDataSource 
         }
     }
 
-    override suspend fun findAll(): List<CategoryModel> {
-        if (_categories.isEmpty()) generateDefaultList()
-        return _categories
-    }
+    override suspend fun findAll(): List<CategoryModel> = _categories
 
     override suspend fun create(category: CategoryModel) {
         category.id = Random().nextLong()
@@ -55,26 +51,5 @@ class CategoryJSONDataSource(private val context: Context) : CategoryDataSource 
     private fun deserialize() {
         val jsonString = read(context, CATEGORIES_JSON_FILE)
         _categories = Json.decodeFromString(jsonString)
-    }
-
-    private suspend fun generateDefaultList() {
-        listOf(
-            CategoryModel(name = "Food", icon = CategoryIcon.EATING),
-            CategoryModel(name = "Clothes", icon = CategoryIcon.CLOTHING),
-            CategoryModel(name = "Others", icon = CategoryIcon.OTHERS),
-            CategoryModel(name = "Entertainment", icon = CategoryIcon.ENTERTAINMENT),
-            CategoryModel(name = "Family", icon = CategoryIcon.FAMILY),
-            CategoryModel(name = "Fuel", icon = CategoryIcon.FUEL),
-            CategoryModel(name = "Gift", icon = CategoryIcon.GIFT),
-            CategoryModel(name = "Groceries", icon = CategoryIcon.GROCERIES),
-            CategoryModel(name = "Rental", icon = CategoryIcon.HOME),
-            CategoryModel(name = "Medical", icon = CategoryIcon.MEDICAL),
-            CategoryModel(name = "Phone bill", icon = CategoryIcon.PHONE_BILL),
-            CategoryModel(name = "Shopping", icon = CategoryIcon.SHOPPING),
-            CategoryModel(name = "Sports", icon = CategoryIcon.SPORTS),
-            CategoryModel(name = "Trip", icon = CategoryIcon.TRAVEL),
-            CategoryModel(name = "Utilities", icon = CategoryIcon.UTILITIES),
-            CategoryModel(name = "Insurance", icon = CategoryIcon.LIFE),
-        ).forEach { create(it) }
     }
 }
--- app/src/main/java/com/nxdmn/xpense/data/repositories/CategoryRepository.kt
+++ app/src/main/java/com/nxdmn/xpense/data/repositories/CategoryRepository.kt
@@ -2,6 +2,7 @@ package com.nxdmn.xpense.data.repositories
 
 import com.nxdmn.xpense.data.dataSources.CategoryDataSource
 import com.nxdmn.xpense.data.models.CategoryModel
+import com.nxdmn.xpense.ui.CategoryIcon
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 
@@ -11,12 +12,15 @@ class CategoryRepository(
     // Mutex to make writes to cached values thread-safe.
     private val getAllCategoriesMutex = Mutex()
 
-    // Cache of the expenseList
+    // Cache of the categoryList
     private var categoryList: List<CategoryModel> = emptyList()
 
     suspend fun getAllCategories(refresh: Boolean = false): List<CategoryModel> {
         if (refresh || categoryList.isEmpty()) {
-            val data = categoryLocalDataSource.findAll()
+            var data = categoryLocalDataSource.findAll()
+
+            if (data.isEmpty()) data = generateDefaultList()
+            
             // Thread-safe write to expenseList
             getAllCategoriesMutex.withLock {
                 this.categoryList = data
@@ -29,4 +33,25 @@ class CategoryRepository(
     suspend fun updateCategory(category: CategoryModel) = categoryLocalDataSource.update(category)
 
     suspend fun createCategory(category: CategoryModel) = categoryLocalDataSource.create(category)
+
+    private suspend fun generateDefaultList(): List<CategoryModel> {
+        return listOf(
+            CategoryModel(name = "Food", icon = CategoryIcon.EATING),
+            CategoryModel(name = "Clothes", icon = CategoryIcon.CLOTHING),
+            CategoryModel(name = "Others", icon = CategoryIcon.OTHERS),
+            CategoryModel(name = "Entertainment", icon = CategoryIcon.ENTERTAINMENT),
+            CategoryModel(name = "Family", icon = CategoryIcon.FAMILY),
+            CategoryModel(name = "Fuel", icon = CategoryIcon.FUEL),
+            CategoryModel(name = "Gift", icon = CategoryIcon.GIFT),
+            CategoryModel(name = "Groceries", icon = CategoryIcon.GROCERIES),
+            CategoryModel(name = "Rental", icon = CategoryIcon.HOME),
+            CategoryModel(name = "Medical", icon = CategoryIcon.MEDICAL),
+            CategoryModel(name = "Phone bill", icon = CategoryIcon.PHONE_BILL),
+            CategoryModel(name = "Shopping", icon = CategoryIcon.SHOPPING),
+            CategoryModel(name = "Sports", icon = CategoryIcon.SPORTS),
+            CategoryModel(name = "Trip", icon = CategoryIcon.TRAVEL),
+            CategoryModel(name = "Utilities", icon = CategoryIcon.UTILITIES),
+            CategoryModel(name = "Insurance", icon = CategoryIcon.LIFE),
+        ).onEach { createCategory(it) }
+    }
 }
