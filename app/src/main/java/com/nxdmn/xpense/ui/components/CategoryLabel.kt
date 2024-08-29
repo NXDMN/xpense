9ae1427ee6d08b3b34986dd0319b290c157626fd
NXDMN <norvintsl@gmail.com>
2024-08-29 23:10:13 +0800

    Remove unused imports

--- app/src/main/java/com/nxdmn/xpense/ui/components/CategoryLabel.kt
+++ app/src/main/java/com/nxdmn/xpense/ui/components/CategoryLabel.kt
@@ -1,6 +1,5 @@
 package com.nxdmn.xpense.ui.components
 
-import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
