6ad4850906f8c1830cb306d795100601b1128dae
NXDMN <norvintsl@gmail.com>
2024-12-19 00:23:24 +0800

    Handle edgetoedge

--- app/src/main/java/com/nxdmn/xpense/MainActivity.kt
+++ app/src/main/java/com/nxdmn/xpense/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -18,6 +19,7 @@ import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -68,6 +70,7 @@ private fun XpenseApp(context: Context) {
         )
 
         Scaffold(
+            contentWindowInsets = WindowInsets(0.dp),
             bottomBar = {
                 BottomAppBar(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
