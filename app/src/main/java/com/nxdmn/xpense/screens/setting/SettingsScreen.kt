a0cc0cc801d78806f895e99132642687e77a919a
NXDMN <norvintsl@gmail.com>
2025-01-21 00:29:04 +0800

    Add category list in SettingsScreen

--- app/src/main/java/com/nxdmn/xpense/screens/setting/SettingsScreen.kt
+++ app/src/main/java/com/nxdmn/xpense/screens/setting/SettingsScreen.kt
@@ -1,14 +1,26 @@
 package com.nxdmn.xpense.screens.setting
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.sharp.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
@@ -21,7 +33,15 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
@@ -31,10 +51,14 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.nxdmn.xpense.R
+import com.nxdmn.xpense.data.models.CategoryModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingsScreen(settingsViewModel: SettingsViewModel = viewModel(factory = SettingsViewModel.Factory)) {
+
+    val settingsUiState by settingsViewModel.uiState.collectAsState()
+
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
@@ -55,24 +79,10 @@ fun SettingsScreen(settingsViewModel: SettingsViewModel = viewModel(factory = Se
                 .verticalScroll(rememberScrollState())
                 .padding(20.dp)
         ) {
-            SettingsListItem(
-                title = "Category",
-                leading = {
-                    Icon(
-                        painterResource(R.drawable.round_category_24),
-                        contentDescription = "Category",
-                        modifier = Modifier.size(36.dp)
-                    )
-                },
-                trailing = {
-                    Icon(
-                        Icons.AutoMirrored.Sharp.KeyboardArrowRight,
-                        contentDescription = "Right Arrow"
-                    )
-                },
-                isTop = true
-            )
-            HorizontalDivider()
+            CategoryList(settingsUiState.categoryList)
+
+            HorizontalDivider(color = Color.LightGray)
+
             SettingsListItem(
                 title = "Currency",
                 leading = {
@@ -122,6 +132,66 @@ fun SettingsListItem(
             trailingContent = trailing,
             colors = ListItemDefaults.colors(containerColor = MaterialTheme.colorScheme.surfaceContainer)
         )
+    }
+}
+
+@Composable
+fun CategoryList(categoryList: List<CategoryModel>) {
+    var isExpanded by remember { mutableStateOf(false) }
+
+    SettingsListItem(
+        title = "Category",
+        leading = {
+            Icon(
+                painterResource(R.drawable.round_category_24),
+                contentDescription = "Category",
+                modifier = Modifier.size(36.dp)
+            )
+        },
+        trailing = {
+            Icon(
+                Icons.AutoMirrored.Sharp.KeyboardArrowRight,
+                contentDescription = "Right Arrow",
+                modifier = Modifier.rotate(if (isExpanded) 90f else 0f)
+            )
+        },
+        isTop = true,
+        onClick = {
+            isExpanded = !isExpanded
+        }
+    )
+    AnimatedVisibility(
+        visible = isExpanded,
+        enter = expandVertically() + fadeIn(),
+        exit = shrinkVertically() + fadeOut()
+    ) {
+        Column {
+            categoryList.forEach {
+                SettingsListItem(
+                    title = it.name,
+                    leading = {
+                        Icon(
+                            painterResource(id = it.icon.resId),
+                            contentDescription = it.name,
+                            modifier = Modifier
+                                .size(36.dp)
+                                .border(
+                                    1.dp, Color(it.color), CircleShape
+                                )
+                                .clip(CircleShape)
+                                .background(Color(it.color))
+                                .padding(5.dp)
+                        )
+                    },
+                    trailing = {
+                        Row(horizontalArrangement = Arrangement.spacedBy(15.dp)) {
+                            Icon(Icons.Filled.Delete, contentDescription = "Delete Category")
+                            Icon(Icons.Filled.Edit, contentDescription = "Edit Category")
+                        }
+                    },
+                )
+            }
+        }
     }
 }
 
--- app/src/main/java/com/nxdmn/xpense/screens/setting/SettingsViewModel.kt
+++ app/src/main/java/com/nxdmn/xpense/screens/setting/SettingsViewModel.kt
@@ -7,13 +7,28 @@ import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.nxdmn.xpense.MainApplication
+import com.nxdmn.xpense.data.models.CategoryModel
 import com.nxdmn.xpense.data.repositories.CategoryRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+
+data class SettingsUiState(
+    val categoryList: List<CategoryModel> = emptyList(),
+)
 
 class SettingsViewModel(repository: CategoryRepository) : ViewModel() {
 
+    private val _uiState = MutableStateFlow(SettingsUiState())
+    val uiState: StateFlow<SettingsUiState> = _uiState.asStateFlow()
+
     init {
         viewModelScope.launch {
+            _uiState.update {
+                it.copy(categoryList = repository.getAllCategories())
+            }
         }
     }
 
