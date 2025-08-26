7d8e2cd459e9a3cc568786b96c5b6371e7f97e2f
NXDMN <norvintsl@gmail.com>
2025-08-27 00:52:19 +0800

    build: Remove kotlinCompilerExtensionVersion in gradle since Kotlin 2.0 use plugins to handle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,9 +89,7 @@ android {
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.12"
-    }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
